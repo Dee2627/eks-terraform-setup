7b25094efc3781e60ef76d019238adfc9cd17a73
Aditya Dayal <adityadayal111@gmail.com>
2025-08-31 13:40:07 +0530

    Initial clean commit

---
+++ eks/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.13.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "aditya-eks-tf-state-bucket"
+    region         = "ap-south-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region = var.aws-region
+}
